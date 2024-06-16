851625379cf94e094da18582304d0a875ac5091c
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-17 02:46:34 +0500

    51st push

--- app.js
+++ app.js
@@ -4,10 +4,44 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import userRoutes from "./routes/user.routes.js";
 import refferalRoutes from "./routes/refferal.routes.js";
-import "./config/db.js";
+// import "./config/db.js";
 import "./middleware/auth.middleware.js";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+    serverSelectionTimeoutMS: 5000
+  },
+  tls: true,
+  tlsAllowInvalidCertificates: false,
+  tlsAllowInvalidHostnames: false,
+});
 
 const app = express();
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
