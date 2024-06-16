771b67dccc8dcca54e2b5f55a18c3f89d8a346e4
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-16 21:56:25 +0500

    30th push

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import userRoutes from "./routes/user.routes.js";
 import refferalRoutes from "./routes/refferal.routes.js";
-import connectDB from "./config/db.js";
 import "./config/db.js";
 import "./middleware/auth.middleware.js";
 
 const app = express();
-
-// Connect to MongoDB
-connectDB();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
--- config/db.js
+++ config/db.js
@@ -3,17 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // useNewUrlParser: true, // No longer needed
-      // useUnifiedTopology: true, // No longer needed
-    });
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // Exit process with failure
-  }
-};
+mongoose.connect(process.env.MONGO_URI, {});
 
-export default connectDB;
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
