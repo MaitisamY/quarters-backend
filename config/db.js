8daaa0ceffbcf6f4f9a07e1a874f6bb4501c288f
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-16 21:17:30 +0500

    10th push

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {});
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
