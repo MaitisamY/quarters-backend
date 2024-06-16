c6775a5cdee977fc6998538e354087612154edf2
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-17 00:33:48 +0500

    mongod added

--- config/db.js
+++ config/db.js
@@ -1,13 +1,28 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-dotenv.config();
+const uri = "mongodb+srv://root:quarters%40123456789@quarters.k9xvyvk.mongodb.net/";
 
-mongoose.connect(process.env.MONGO_URI, {});
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
