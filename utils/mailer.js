4c917c4034a0a493b8f52e994ec414a7cc02ef95
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-17 22:00:08 +0500

    150th push

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,8 +5,7 @@ import { getEmailTemplate } from "./emailTemplates.js"; // New import
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
