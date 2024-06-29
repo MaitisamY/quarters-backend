11ab4b921841831a3f8a72a070e83f99a25e59e2
Muhammad Aitisam Yaseen <geeker.one.afu.systems@gmail.com>
2024-06-29 16:12:24 +0500

    update referrals method

--- services/referral.service.js
+++ services/referral.service.js
@@ -1,20 +1,30 @@
 import Referral from "../models/referral.model.js";
+import User from "../models/user.model.js";
 import { sendReferralEmail } from "../utils/referralEmail.js";
 
 /* Referral service methods */
 export const ReferralService = {
     referrals: async (userData) => {
         try {
-            // console.log('Processing referral with data:', userData); <--- Uncomment to debug
+            console.log('Processing referral with data:', userData);
 
+            // Check if the referred email is already registered
+            const existingUser = await User.findOne({ email: userData.referred_email }).maxTimeMS(30000);
+            if (existingUser) {
+                throw new Error("This email is already registered");
+            }
+
+            // Check if the referred email is already in the referrals
             const existingEmail = await Referral.findOne({ referred_email: userData.referred_email }).maxTimeMS(30000);
             if (existingEmail) {
                 throw new Error("This email has already been referred");
             }
 
+            // Save the referral
             const referral = new Referral({ ...userData });
             await referral.save();
 
+            // Send referral email
             sendReferralEmail(userData.referrer, userData.referred_email, userData.referral_code);
 
             return { message: "Referral sent successfully" };
